1b870f438d62cfeb2013e13e296451d7bc3995ef
Valiantsin Rakush <vrakush.external@epo.org>
2022-09-01 21:00:03 +0300

    use class

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  console.log('rendered');
-  return (
-    <div>Hi there!</div>
-  );
-};
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err)
+//   );
+//   console.log('rendered');
+//   return (
+//     <div>Hi there!</div>
+//   );
+// };
 
+class App extends React.Component {
+
+  render = () => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    )
+
+    return (
+      <div>Position:</div>
+    );
+  }
+
+};
 
 const container = document.getElementById('root');
 const root = createRoot(container);
